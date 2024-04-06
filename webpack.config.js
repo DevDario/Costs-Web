d7e8a0c164272630d1c83f97fa5ff5c1dbaa9ab4
Dário Silva <dariosilva13222@gmail.com>
2024-04-06 13:29:28 +0100

    build(fix): removes invalid entry-point

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
     entry: {
         editProjects: './scripts/CRUD/edit.js',
-        login: './scripts/userAuth.js',
         newProject: './scripts/CRUD/create.js',
         viewProjects: './scripts/CRUD/RUD.js',
     },
