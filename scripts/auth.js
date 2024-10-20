ef252c85bf8e1d80c6c22913853df23f9c65ee3a
Dário Silva <dariosilva13222@gmail.com>
2024-10-20 14:40:50 +0100

    feat: sets event listeners to both github and google login buttons

---
+++ scripts/auth.js
@@ -0,0 +1,10 @@
+const githubLoginButton = document.getElementById("github-login")
+const googleLoginButton = document.getElementById("google-login")
+
+githubLoginButton.addEventListener('click',()=>{
+    alert("github")
+})
+
+googleLoginButton.addEventListener('click',()=>{
+    alert("google")
+})
