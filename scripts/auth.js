21f9e0e36fb027340bbd8c8e50eeb3c357e006cd
Dário Silva <dariosilva13222@gmail.com>
2024-11-02 17:05:27 +0100

    refactor: fixes authentication redirect link

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,6 +1,6 @@
 const githubLoginButton = document.getElementById("github-login")
 const googleLoginButton = document.getElementById("google-login")
-const authBaseURL = "http://localhost:8081/oauth2/authorization/"
+const authBaseURL = "http://localhost:8081/oauth2/authorization"
 
 githubLoginButton.addEventListener('click',()=>{
     window.location.href = `${authBaseURL}/github`
