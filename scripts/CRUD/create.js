59d6ba1ddd2cb9e1005d189de958cfcbea9f006d
Dário Silva <dariosilva13222@gmail.com>
2024-11-10 21:33:16 +0100

    refactor: formates file using eslint

--- scripts/CRUD/create.js
+++ scripts/CRUD/create.js
@@ -7,71 +7,71 @@ createProjectButton.addEventListener('click', (event) => {
     event.preventDefault()
 
     const projectDetails = {
-        name: document.getElementById('project-name').value,
+        name: String(document.getElementById('project-name').value),
         budget: parseInt(document.getElementById('project-budget').value),
-        category: document.getElementById('category').value,
+        category: String(document.getElementById('category').value),
         usedBudget: 0.0,
         deadline: setProjectDeadline(),
     }
 
-    if (!projectDetails.name)  alert("Project Name missing !")
-    if (!projectDetails.budget)  alert("Project Budget missing !")
-    if (projectDetails.category === "none")  return alert("Project Category missing !")
-    if(projectDetails.deadline === false) return 
+    if (!projectDetails.name) alert("Project Name missing !")
+    if (!projectDetails.budget) alert("Project Budget missing !")
+    if (projectDetails.category === "none") return alert("Project Category missing !")
+    if (projectDetails.deadline === false) return
 
-    fetch(`${apiBaseURL}/project/new`,{
-        headers:{
-            "Content-Type":"application/json"
+    fetch(`${apiBaseURL}/project/new`, {
+        headers: {
+            "Content-Type": "application/json"
         },
-        method:"POST",
+        method: "POST",
         body: JSON.stringify(projectDetails),
-        credentials:'include'
+        credentials: 'include'
     })
-    .then((response)=>{
-        if(response.ok){
-            alert("Project Created Successfully !")
+        .then((response) => {
+            if (response.ok) {
+                alert("Project Created Successfully !")
 
-            // redirecting user to view projects page
-            window.location.href = "http://localhost:3333/ViewProjects/viewprojects.html"
-        }else{
-            alert("We couldnt create your project. Try to reload and submit again")
+                // redirecting user to view projects page
+                window.location.href = "http://localhost:3333/ViewProjects/viewprojects.html"
+            } else {
+                alert("We couldnt create your project. Try to reload and submit again")
 
-            if(response.status===400){
-                console.log(`something went sounth ! -> ${response.message}`)
+                if (response.status === 400) {
+                    console.log(`something went sounth ! -> ${response.message}`)
+                }
+
+                console.log(`Error -> ${response.message}`)
             }
-
-            console.log(`Error -> ${response.message}`)
-        }
-    })
-    .catch((error)=>{
-        console.error(`Catched this -> ${error}`)
-    })
+        })
+        .catch((error) => {
+            console.error(`Catched this -> ${error}`)
+        })
 
 })
 
-function setProjectDeadline(){
-    
+function setProjectDeadline() {
+
     const deadline = document.getElementById('project-deadline').value
     const dateFormat = /^\d{4}-\d{2}-\d{2}$/
-    
-    if(deadline.match(dateFormat)){
+
+    if (deadline.match(dateFormat)) {
         //checks if the deadline is a future date
         const currentDate = new Date()
 
         const selectedDate = new Date(deadline)
 
-        if(selectedDate > currentDate){
+        if (selectedDate > currentDate) {
 
             return selectedDate.toISOString().replace('Z', '+00:00');
-            
-        }else{
+
+        } else {
             alert("Please enter a future date")
             return false
         }
 
-    }else{
+    } else {
 
-        alert("Please enter a valid date in the format YYYY-MM-DD.") 
+        alert("Please enter a valid date in the format YYYY-MM-DD.")
         return false
 
     }
